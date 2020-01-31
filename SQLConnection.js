5806d32dddc725a29ebf1800cae108beda87e0a6
Matthew Golden <matthew@wgolden.com>
2020-01-31 06:47:15 -0600

    Added server side pagination for AdventureWorks Catalog. Updated Catalog router, and changes SQL call to a sproc on DB

--- SQLConnection.js
+++ SQLConnection.js
@@ -1,38 +1,36 @@
 const Connection = require('tedious').Connection
 const Request = require('tedious').Request
+const TYPES = require('tedious').TYPES;
 const config = require('./.sql-connection.json')
 
 
-function getProducts(res) {
+function getProducts(res, page, rows) {
   var connection = new Connection(config)
 
   connection.on('connect', function(err){
-    if(err){
-      console.log(`There was an issue: ${err}`)
-    }else{
-      console.log('Connected to database')
-      const sql = 'SELECT ProductID, Name, Color, StandardCost,ListPrice,Size,Weight,DaysToManufacture  FROM Production.Product'
-      var request = new Request(sql, function(err, rowCount, rows){
-        let resultSet = []
-        let columnHeaders = []
-        rows.forEach( row  => {
-          let product = {}
-          row.forEach(column => {
-            if(!columnHeaders.includes(column.metadata.colName)){
-                columnHeaders.push(column.metadata.colName)
-            }
-            product[column.metadata.colName] = column.value
-          })
-          resultSet.push(product)
+    const request = new Request('spGetProducts', function(err, rowCount, rows){
+      if(err){
+        console.log(err)
+      }
+      let resultSet = []
+      let columnHeaders = []
+      rows.forEach( row  => {
+        let product = {}
+        row.forEach(column => {
+          if(!columnHeaders.includes(column.metadata.colName)){
+              columnHeaders.push(column.metadata.colName)
+          }
+          product[column.metadata.colName] = column.value
         })
-        //console.log(resultSet)
-        res.render('products',{products: {headers: columnHeaders, data: resultSet}})
+        resultSet.push(product)
       })
-      request.on('requestCompleted', function(){
-        connection.close()
-      })
-      connection.execSql(request)
-    }
+      //console.log(resultSet)
+      res.render('products',{products: {headers: columnHeaders, data: resultSet}})
+      connection.close()
+    })
+    request.addParameter('page', TYPES.Int, page)
+    request.addParameter('rows', TYPES.Int, rows)
+    connection.callProcedure(request)
   })
 }
 
--- index.js
+++ index.js
@@ -1,15 +1,32 @@
 const express = require('express')
 const sqlConnection = require('./SQLConnection')
-const exphbs  = require('express-handlebars');
+const handlebars  = require('express-handlebars');
 const app = express()
 const port = 3000
 
-app.engine('handlebars', exphbs())
+
+const exphbs = handlebars.create({
+  helpers:{
+    dataCount: function(data){return data.length},
+    return10: function(data){return data.slice(0, 9)}
+  }
+})
+app.engine('handlebars', exphbs.engine)
 app.set('view engine', 'handlebars')
 app.use(express.static(__dirname + '/public'))
 
-app.get('/', (req, res) => {
-  sqlConnection.getProducts(res)
+app.get('/Catalog', (req, res) => {
+  let page = req.query.page
+  let rows = req.query.rows
+  console.log(`page: ${page}`)
+  console.log(`rows: ${rows}`)
+  if(page === undefined){
+    page = 1
+  }
+  if(rows === undefined){
+    rows = 10
+  }
+  sqlConnection.getProducts(res, page, rows)
 })
 
 app.listen(port, () => console.log(`App listening on port ${port}`))
