271f044d25e6ecfc529068c8a7bc644bb6df7d4f
Matthew Golden <matthew@wgolden.com>
2020-02-02 06:47:34 -0600

    Updated route to Products display. Updated SQL call to a stored procedure that uses OFFSET/FETCH to paginate the data. Added results range display to page, and buttons to move to previous and next pages.

--- SQLConnection.js
+++ SQLConnection.js
@@ -4,32 +4,57 @@ const TYPES = require('tedious').TYPES;
 const config = require('./.sql-connection.json')
 
 
-function getProducts(res, page, rows) {
+function getProducts(res, page, rowsPerPage) {
   var connection = new Connection(config)
-
   connection.on('connect', function(err){
     const request = new Request('spGetProducts', function(err, rowCount, rows){
       if(err){
         console.log(err)
       }
+      let columnHeaders = rows[0].map((column) => {
+        return column.metadata.colName
+      })
+      columnHeaders.pop() //removing totalcount from row headers
+
+      let totalResults = rows[0].filter((column) =>{
+        if(column.metadata.colName === 'TotalCount'){
+          return column
+        }
+      })[0]
+
       let resultSet = []
-      let columnHeaders = []
       rows.forEach( row  => {
         let product = {}
         row.forEach(column => {
-          if(!columnHeaders.includes(column.metadata.colName)){
-              columnHeaders.push(column.metadata.colName)
+          if(columnHeaders.includes(column.metadata.colName)){
+            product[column.metadata.colName] = column.value
           }
-          product[column.metadata.colName] = column.value
         })
         resultSet.push(product)
       })
-      //console.log(resultSet)
-      res.render('products',{products: {headers: columnHeaders, data: resultSet}})
+      res.render('products',
+            {
+                products:
+                  { headers: columnHeaders,
+                    data: resultSet,
+                    CurrentResultCount: resultSet.length,
+                    CurrentPage: page,
+                    RowsPerPage: rowsPerPage,
+                    TotalResultCount: totalResults.value,
+                    TotalPages: Math.ceil(totalResults.value / rowsPerPage)
+                  },
+                helpers: {
+                  previousPage: function(CurrentPage){return page - 1},
+                  nextPage: function(CurrentPage){return parseInt(page) + 1},
+                  outputRange: function(CurrentPage, RowsPerPage) {
+                      return `Results ${1 + (page - 1) * rowsPerPage} - ${(page - 1) * rowsPerPage + parseInt(rowsPerPage)}`
+                  }
+                }
+              })
       connection.close()
     })
     request.addParameter('page', TYPES.Int, page)
-    request.addParameter('rows', TYPES.Int, rows)
+    request.addParameter('rows', TYPES.Int, rowsPerPage)
     connection.callProcedure(request)
   })
 }
--- index.js
+++ index.js
@@ -5,12 +5,7 @@ const app = express()
 const port = 3000
 
 
-const exphbs = handlebars.create({
-  helpers:{
-    dataCount: function(data){return data.length},
-    return10: function(data){return data.slice(0, 9)}
-  }
-})
+const exphbs = handlebars.create()
 app.engine('handlebars', exphbs.engine)
 app.set('view engine', 'handlebars')
 app.use(express.static(__dirname + '/public'))
@@ -18,8 +13,6 @@ app.use(express.static(__dirname + '/public'))
 app.get('/Catalog', (req, res) => {
   let page = req.query.page
   let rows = req.query.rows
-  console.log(`page: ${page}`)
-  console.log(`rows: ${rows}`)
   if(page === undefined){
     page = 1
   }
