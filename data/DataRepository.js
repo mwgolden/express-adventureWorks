e42a5cdd5b2169f40f2426e0db0d29d3c9ea32f0
Matthew Golden <matthew@wgolden.com>
2020-02-09 09:10:44 -0600

    Cleaned up data repository and catalog routes

--- data/DataRepository.js
+++ data/DataRepository.js
@@ -5,76 +5,31 @@ const Request = require('tedious').Request
 const TYPES = require('tedious').TYPES;
 
 
-
-
-function getProducts(page, rowsPerPage){
-  return new Promise((resolve, reject) =>{
-    conn = db.DBConnection()
-
-    let data = []
-    let totalProducts = 0
-    let headers = []
-    const request = new Request('spGetProducts', function(err, rowCount){
-      if(err){
-        console.log(err)
-      }
-    })
-    request.addParameter('page', TYPES.Int, page)
-    request.addParameter('rows', TYPES.Int, rowsPerPage)
-
-    request.on('row', function(columns){
-      let result = {}
-      columns.map(column => {
-        result[column.metadata.colName] = column.value
-      })
-      const p = product.Product(
-        result.ProductID, result.Name, result.StandardCost, result.ListPrice,
-        result.Size, result.Weight, result.DaysToManufacture
-      )
-      totalProducts = result.TotalCount
-      data.push(p)
-    })
-    conn.then(
-      conn => { conn.callProcedure(request) }
-    )
-    request.on('requestCompleted', function () {
-      const returnObj = {
-          headers: Object.keys(data[0]),
-          data: data,
-          CurrentResultCount: data.length,
-          CurrentPage: page,
-          RowsPerPage: rowsPerPage,
-          TotalResultCount: totalProducts,
-          TotalPages: Math.ceil(totalProducts / rowsPerPage)
-      }
-      resolve(returnObj)
-    });
-  })
-}
-function getProducts2(page, rows){
+function getProducts(page, rows){
   return new Promise((resolve, reject) => {
     products = []
     conn = db.DBConnection()
     conn.then(
       conn => {
         reqParams = [
-          {
-            Name: 'page',
-            Type: TYPES.Int,
-            Value: page
-          },
-          {
-            Name: 'rows',
-            Type: TYPES.Int,
-            Value: rows
-          }
+          { Name: 'page', Type: TYPES.Int, Value: page },
+          { Name: 'rows', Type: TYPES.Int, Value: rows }
         ]
         request = req.callStoredProcedure(conn, 'spGetProducts', reqParams)
         request.then(
           result => {
-            products = result
+            products = SqlResultToProducts(result)
+            const returnObj = {
+                headers: Object.keys(products.products[0]),
+                data: products.products,
+                CurrentResultCount: products.products.length,
+                CurrentPage: page,
+                RowsPerPage: rows,
+                TotalResultCount: products.TotalCount,
+                TotalPages: Math.ceil(products.TotalCount / rows)
+            }
             conn.close()
-            resolve(products)
+            resolve(returnObj)
           }
         )
       }
@@ -82,5 +37,26 @@ function getProducts2(page, rows){
   })
 }
 
-exports.getProducts = getProducts
-exports.products = getProducts2
+function SqlResultToProducts(sqlResults){
+  let productResult = {}
+  let products = []
+  let totalProductCount = 0
+  sqlResults.forEach(row => {
+    let r = {}
+    row.forEach( column => {
+      r[column.metadata.colName] = column.value
+    });
+    const p = product.Product(
+      r.ProductID, r.Name, r.StandardCost, r.ListPrice,
+      r.Size, r.Weight, r.DaysToManufacture
+    )
+    products.push(p)
+    totalProductCount = r.TotalCount
+  });
+
+  productResult.products = products
+  productResult.TotalCount = totalProductCount
+  return productResult
+}
+
+exports.products = getProducts
--- index.js
+++ index.js
@@ -12,29 +12,23 @@ app.set('view engine', 'handlebars')
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', (req, res) => {
-  p = repo.products(15, 25)
-  p.then(
-    response => {
-      res.send(response)
-    }
-  )
-
-
+  res.send('Hello Adventureworks')
 })
 
 app.get('/Catalog', (req, res) => {
   let page = req.query.page === undefined ? 1 : req.query.page
   let rows = req.query.rows === undefined ? 10 : req.query.rows
 
-  const productResult = repo.getProducts(page, rows)
-  productResult.then(
-    catalog => { res.render('products', {
-      products: catalog,
-      helpers: {
-        previousPage: function(CurrentPage){return page - 1},
-        nextPage: function(CurrentPage){return parseInt(page) + 1},
+  getCatalogPage = repo.products(page, rows)
+  getCatalogPage.then(
+    page => {
+      res.render('products', {
+        products: page,
+        helpers: {
+        previousPage: function(CurrentPage){return parseInt(CurrentPage) - 1},
+        nextPage: function(CurrentPage){return parseInt(CurrentPage) + 1},
         outputRange: function(CurrentPage, RowsPerPage) {
-            return `Results ${1 + (page - 1) * RowsPerPage} - ${(page - 1) * RowsPerPage + parseInt(RowsPerPage)}`
+            return `Results ${1 + (CurrentPage - 1) * RowsPerPage} - ${(CurrentPage - 1) * RowsPerPage + parseInt(RowsPerPage)}`
         }
       }
     })
